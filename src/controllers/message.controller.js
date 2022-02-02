b7e2360636d3a0a16adce07ac22afe929f3f5e8f
8-30 <canino9242@hotmail.com>
2022-02-01 20:44:39 -0500

    Moved db to database folder

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,13 +1,8 @@
 
 const Message = require('../models/mensajeModel')
-const { JsonDB } = require ('node-json-db');
-const { Config } = require ('node-json-db/dist/lib/JsonDBConfig')
+const db = require('../database/databaseJSON')
 
-const db = new JsonDB(new Config("myDataBase", true, false, '/'));
-
-const messageStorage = db.getData("/messages");
-
-
+const messageStorage = db.getData("/messages")
 
 const getMessages = (req, res) =>{
     res.json(messageStorage)
@@ -15,10 +10,12 @@ const getMessages = (req, res) =>{
 
 const postMessage = (req, res) =>{
     const {sender,text} = req.body
+
     const message = new Message(text,sender)
     db.push("/messages[]", 
         message
-    , true);
+    , true)
+
     res.json(messageStorage)
 }
 
